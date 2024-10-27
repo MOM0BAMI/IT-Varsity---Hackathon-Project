5f9460576c8c80b56230eebe32f8d89a6e55819b
William-Daniel-Maistry <savedbygrace.wdm@gmail.com>
2024-10-27 02:13:03 +0200

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,14 @@
 // src/components/Header.js
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
-    <header className="header-container">
-      <h1 className="header-title">Telemedicine App</h1>
+    <header>
+      <h1>Telemedicine App</h1>
       <nav>
-        <ul className="nav-list">
+        <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
@@ -17,11 +16,7 @@ const Header = () => {
           <li><Link to="/patient-data">Patient Data</Link></li>
           <li><Link to="/emergency-alerts">Emergency Alerts</Link></li>
           <li><Link to="/emergency-contact">Emergency Contacts</Link></li>
-          <li><Link to="/symptom-checker">Symptom Checker</Link></li>
-          <li><Link to="/health-log">Health Log</Link></li>
-          <li><Link to="/health-resources">Health Resources</Link></li>
-          <li><Link to="/medicine-locator">Medicine Locator</Link></li>
-          <li><Link to="/chatbot">Chatbot</Link></li> {/* Added Chatbot link */}
+          <li><Link to="/symptom-checker">Symptom Checker</Link></li> {/* Link for Symptom Checker */}
         </ul>
       </nav>
     </header>
